be930615f8e67fa34137ad1fbcc67285cffe195c
EkkoG <beijiu572@gmail.com>
2023-04-23 19:34:59 +0800

    Refine golang comiler use

--- patch.sh
+++ patch.sh
@@ -1,17 +1,7 @@
 #!/bin/bash -e
-function sed_wrapper() {
-    # if run in linux, use sed -i
-    # if run in macos, use sed -i ''
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        sed -i "$@"
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        /usr/local/opt/gnu-sed/libexec/gnubin/sed -i "$@"
-    fi
-}
+export PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
+find . -name 'Makefile' -not -path './packages/golang/*' -exec sed  -i '/golang-package.mk/ c\include ../golang/golang-package.mk' {} \;
 
-sed_wrapper '/golang-package.mk/ c\include ../golang/golang-package.mk' packages/mosdns/Makefile
-sed_wrapper '/golang-package.mk/ c\include ../golang/golang-package.mk' packages/clash-for-openclash/Makefile
-sed_wrapper '/golang-package.mk/ c\include ../golang/golang-package.mk' packages/clash-meta-for-openclash/Makefile
 if [[ $1 =~ '21.02'* ]]; then
     find . -name 'Makefile' -exec sed -i '/PKG_BUILD_FLAGS:=no-mips16/ c\PKG_USE_MIPS16:=0' {} \;
 else
