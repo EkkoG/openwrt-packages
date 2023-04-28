7ec935ca66d15681e5f9736bb026b1d46a3d1a19
EkkoG <beijiu572@gmail.com>
2023-04-28 22:39:25 +0800

    Fixed wrong patch for snapshot

--- patch.sh
+++ patch.sh
@@ -2,9 +2,8 @@
 export PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
 find . -name 'Makefile' -not -path './packages/golang/*' -exec sed  -i '/golang-package.mk/ c\include ../golang/golang-package.mk' {} \;
 
-find . -name 'Makefile' -exec sed -i '/PKG_BUILD_FLAGS:=no-mips16/ c\PKG_USE_MIPS16:=0' {} \;
-# if [[ $1 =~ '21.02'* ]]; then
-#     find . -name 'Makefile' -exec sed -i '/PKG_BUILD_FLAGS:=no-mips16/ c\PKG_USE_MIPS16:=0' {} \;
-# else
-#     find . -name 'Makefile' -exec sed -i '/PKG_USE_MIPS16:=0/ c\PKG_BUILD_FLAGS:=no-mips16' {} \;
-# fi
+if [[ $1 =~ 'SNAPSHOT'* ]]; then
+    find . -name 'Makefile' -exec sed -i '/PKG_USE_MIPS16:=0/ c\PKG_BUILD_FLAGS:=no-mips16' {} \;
+else
+    find . -name 'Makefile' -exec sed -i '/PKG_BUILD_FLAGS:=no-mips16/ c\PKG_USE_MIPS16:=0' {} \;
+fi
