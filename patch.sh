28c8be83e8d7e4636a70313f91c2cb5b799491f9
EkkoG <beijiu572@gmail.com>
2023-12-11 14:40:53 +0800

    Add mipsel_24kc

--- patch.sh
+++ patch.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
 find . -name 'Makefile' -not -path './packages/golang/*' -exec sed  -i '/golang-package.mk/ c\include ../golang/golang-package.mk' {} \;
 
-if [[ $1 =~ 'SNAPSHOT'* ]]; then
+if [[ $1 =~ 'SNAPSHOT'* || $1 =~ '23.05.' ]]; then
     find . -name 'Makefile' -exec sed -i '/PKG_USE_MIPS16:=0/ c\PKG_BUILD_FLAGS:=no-mips16' {} \;
 else
     find . -name 'Makefile' -exec sed -i '/PKG_BUILD_FLAGS:=no-mips16/ c\PKG_USE_MIPS16:=0' {} \;
